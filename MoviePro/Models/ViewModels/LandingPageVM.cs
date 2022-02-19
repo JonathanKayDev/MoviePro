4a811d6fd18a9562d8dc523201921fe8de8c066c
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-02-19 11:38:04 +0000

    Create LandingPageVM.cs

---
+++ MoviePro/Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,16 @@
+﻿using MoviePro.Models.Database;
+using MoviePro.Models.TMDB;
+
+namespace MoviePro.Models.ViewModels
+{
+    // LandingPageVM is an aggregate of Collection Model and MovieSearch Model
+    public class LandingPageVM
+    {
+        public List<Collection>? CustomCollections { get; set; }
+        // Each property to hold it's own MovieSearch info, so they can be independently displayed on the landing page
+        public MovieSearch? NowPlaying { get; set; }
+        public MovieSearch? Popular { get; set; }
+        public MovieSearch? TopRated { get; set; }
+        public MovieSearch? Upcoming { get; set; }
+    }
+}
