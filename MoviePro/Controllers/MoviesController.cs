9eb6f637925b2ef14c19ee6a9443b20fb6fb36f4
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-02-22 12:20:30 +0000

    Add Details action to MoviesController

--- MoviePro/Controllers/MoviesController.cs
+++ MoviePro/Controllers/MoviesController.cs
@@ -65,7 +65,37 @@ namespace MoviePro.Controllers
             return View(movies);
         }
 
+        public async Task<IActionResult> Details(int? id, bool local = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            Movie movie = new Movie();
+            if (local)
+            {
+                // Get the movie data straight from the Db
+                movie = await _context.Movie.Include(m => m.Cast)
+                                            .Include(m => m.Crew)
+                                            .FirstOrDefaultAsync(m => m.Id == id);
+            }
+            else
+            {
+                // Get the movie data from the TMDB API
+                var movieDetail = await _tmdbMovieService.MovieDetailAsync((int)id);
+                movie = await _tmdbMappingService.MapMovieDetailAsync(movieDetail);
+            }
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Local"] = local;
+
+            return View(movie);
+        }
 
         // GET: Temp/Create
         public IActionResult Create()
