59099151137f51bd231511e0e7adb83a594c74f3
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-02-22 16:38:04 +0000

    Add MovieCollectionsController

---
+++ MoviePro/Controllers/MovieCollectionsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MoviePro.Data;
+using MoviePro.Models.Database;
+
+namespace MoviePro.Controllers
+{
+    public class MovieCollectionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieCollectionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            // if no id provided, default to the ALL collection
+            id ??= (await _context.Collection.FirstOrDefaultAsync(c => c.Name.ToUpper() == "ALL")).Id;
+
+            ViewData["CollectionId"] = new SelectList(_context.Collection,"Id","Name", id);
+
+            var allMovieIds = await _context.Movie.Select(m => m.Id).ToListAsync();
+
+            var movieIdsInCollection = await _context.MovieCollection
+                                        .Where(m => m.CollectionId == id)
+                                        .OrderBy(m => m.Order)
+                                        .Select(m => m.MovieId)
+                                        .ToListAsync();
+
+            var movieIdsNotInCollection = allMovieIds.Except(movieIdsInCollection);
+
+            // Create a new list and add movies in the collection in the specified order
+
+            var moviesInCollection = new List<Movie>();
+            // For each id in collection add to new list
+            movieIdsInCollection.ForEach(movieId => moviesInCollection.Add(_context.Movie.Find(movieId)));
+
+            ViewData["IdsInCollection"] = new MultiSelectList(moviesInCollection, "Id", "Title");
+
+            // Declare local variable and set to records with id not in the collection (not tracked by entity framework)
+            var moviesNotInCollection = await _context.Movie.AsNoTracking().Where(m => movieIdsNotInCollection.Contains(m.Id)).ToListAsync();
+            ViewData["IdsNotInCollection"] = new MultiSelectList(moviesNotInCollection,"Id","Title");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(int id, List<int> idsInCollection)
+        {
+            // Remove the old movies
+            var oldRecords = _context.MovieCollection.Where(c => c.CollectionId == id);
+            _context.MovieCollection.RemoveRange(oldRecords);
+            await _context.SaveChangesAsync();
+
+            // Add new movies
+            if (idsInCollection != null)
+            {
+                int index = 1;
+                idsInCollection.ForEach(movieId =>
+               {
+                   _context.Add(new MovieCollection()
+                   {
+                       CollectionId = id,
+                       MovieId = movieId,
+                       Order = index++
+                   });
+               });
+
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index), new { id });
+        }
+    }
+}
