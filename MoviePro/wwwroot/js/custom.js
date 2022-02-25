fbdd49824f8354741b3d68630743cd1bc34766dc
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-02-25 18:47:45 +0000

    Add search functionality to movie import view

--- MoviePro/wwwroot/js/custom.js
+++ MoviePro/wwwroot/js/custom.js
@@ -12,7 +12,7 @@
                 wildcard: '%SEARCH',                // %SEARCH will be replaced by users input in the url option.
                 transform: function (response) {    // This function will run before passing the results back to the page
                     let arr = response.results;
-                    // Sort results by popularity
+                    //Sort results by popularity
                     arr.sort((a, b) => {
                         return a.vote_average > b.vote_average ? -1 : 1;
                     });
@@ -54,120 +54,5 @@
         })
     }
 
-    /*-------------------- Movie Collections --------------------*/
-    $('#Form').on('submit', onSubmit);
-
-    function onSubmit() {
-        let collectionIndex = document.getElementById("collection-id").selectedIndex;
-        if (collectionIndex == -1) {
-            document.getElementById("collection-id").selectedIndex = 0;
-            console.log(collectionIndex);
-        }
-
-        let select = document.getElementById("idsInCollection").getElementsByTagName("option");
-
-        for (let option of select) {
-            option.selected = "selected";
-        }
-    }
-
-    $(document).on("click", function (event) {
-        let $trigger = $("#idsInCollection, #idsNotInCollection");
-        if ($trigger !== event.target && !$trigger.has(event.target).length) {
-            $("#idsInCollection, #idsNotInCollection").prop("selectedIndex", -1);
-        }
-    });
-
-    $('#addToCollection').click(function (e) {
-        var selItem = $('#idsNotInCollection').prop('selectedIndex');
-        console.log(selItem);
-        if (selItem == -1) {
-            //window.alert("You must first select a movie not in the collection.")
-        } else {
-            // Add to new list
-            $('#idsInCollection').append($('#idsNotInCollection')[0][selItem]);
-            // Remove from original list
-            let selected = $('#idsNotInCollection').val();
-            $(`#idsNotInCollection option[value=${selected}]`).remove();
-            // Uncheck selected items
-            $('#idsNotInCollection').prop('selectedIndex', -1);
-            $('#idsInCollection').prop('selectedIndex', -1);
-        }
-    });
-
-    $('#removeFromCollection').click(function (e) {
-        var selItem = $('#idsInCollection').prop('selectedIndex');
-        if (selItem == -1) {
-            //window.alert("You must first select a movie in the collection.")
-        } else {
-            // Add to new list
-            $('#idsNotInCollection').append($('#idsInCollection')[0][selItem]);
-            // Remove from original list
-            let selected = $('#idsInCollection').val();
-            $(`#idsInCollection option[value=${selected}]`).remove();
-            // Uncheck selected items
-            $('#idsNotInCollection').prop('selectedIndex', -1);
-            $('#idsInCollection').prop('selectedIndex', -1);
-        }
-    });
-
-    $('#moveUpIn').click(function () {
-        let arr = $('#idsInCollection').children().toArray();
-        let selItem = $('#idsInCollection').prop('selectedIndex');
-
-        if (selItem == -1 || selItem == 0) {
-            //window.alert("You must first select a movie in the collection.")
-        } else {
-            let temp = arr[selItem];
-            arr[selItem] = arr[selItem - 1];
-            arr[selItem - 1] = temp;
-
-            $('#idsInCollection').empty().append(arr);
-        }
-    });
-
-    $('#moveUpNotIn').click(function () {
-        let arr = $('#idsNotInCollection').children().toArray();
-        let selItem = $('#idsNotInCollection').prop('selectedIndex');
-
-        if (selItem == -1 || selItem == 0) {
-            //window.alert("You must first select a movie in the collection.")
-        } else {
-            let temp = arr[selItem];
-            arr[selItem] = arr[selItem - 1];
-            arr[selItem - 1] = temp;
-
-            $('#idsNotInCollection').empty().append(arr);
-        }
-    });
-
-    $('#moveDownIn').click(function () {
-        let arr = $('#idsInCollection').children().toArray();
-        let selItem = $('#idsInCollection').prop('selectedIndex');
-
-        if (selItem == -1 || selItem == arr.length - 1) {
-            //window.alert("You must first select a movie in the collection.")
-        } else {
-            let temp = arr[selItem];
-            arr[selItem] = arr[selItem + 1];
-            arr[selItem + 1] = temp;
-
-            $('#idsInCollection').empty().append(arr);
-        }
-    });
-
-    $('#moveDownNotIn').click(function () {
-        let arr = $('#idsNotInCollection').children().toArray();
-        let selItem = $('#idsNotInCollection').prop('selectedIndex');
-
-        if (selItem == -1 || selItem == arr.length - 1) {
-            //window.alert("You must first select a movie in the collection.")
-        } else {
-            let temp = arr[selItem];
-            arr[selItem] = arr[selItem + 1];
-            arr[selItem + 1] = temp;
-
-            $('#idsNotInCollection').empty().append(arr);
-        }
-    });
+   
 });
