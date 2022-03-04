6de320b07ce8c2b4466878f836e844f8d421dff2
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-03-04 21:16:53 +0000

    Add backtotop function
    Add backtotop function

--- MoviePro/wwwroot/js/custom.js
+++ MoviePro/wwwroot/js/custom.js
@@ -140,4 +140,8 @@ function SelectAllMovies() {
     }
 }
 
-   
+/*-------------------- Navigation --------------------*/
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
