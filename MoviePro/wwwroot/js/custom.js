299138badf3ab47f1da64818eb3efa311306db57
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-03-05 16:08:45 +0000

    Update import search
    Update import search. Fixed bug, where typeahead search view was corrupt and duplicated

--- MoviePro/wwwroot/js/custom.js
+++ MoviePro/wwwroot/js/custom.js
@@ -141,7 +141,7 @@ function SelectAllMovies() {
 }
 
 /*-------------------- Navigation --------------------*/
-function topFunction() {
+function ToTopFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
